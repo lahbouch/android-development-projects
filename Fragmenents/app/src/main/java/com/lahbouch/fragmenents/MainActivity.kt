58a8578ebf7e9918219893d7a37eff65102bb3d7
lahbouch <lahbouch.dev@gmail.com>
2023-02-01 01:00:12 +0100

    initial fragment project

---
+++ Fragmenents/app/src/main/java/com/lahbouch/fragmenents/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.lahbouch.fragmenents
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
