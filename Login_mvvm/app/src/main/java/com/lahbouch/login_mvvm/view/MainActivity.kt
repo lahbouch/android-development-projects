33579fffe44155bdc8001e0e4c17e94bf68257b9
lahbouch <lahbouch.dev@gmail.com>
2023-02-05 00:35:23 +0100

    ui complete

---
+++ Login_mvvm/app/src/main/java/com/lahbouch/login_mvvm/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.lahbouch.login_mvvm.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.lahbouch.login_mvvm.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
