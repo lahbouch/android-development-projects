f4ea095c416326baff4319455739b5d290d10976
lahbouch <lahbouch.dev@gmail.com>
2023-02-05 02:31:04 +0100

    initial commit with navigation bar

---
+++ Navigation_menu/app/src/main/java/com/lahbouch/navigation_menu/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.lahbouch.navigation_menu
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        supportActionBar?.hide()
+    }
+}
